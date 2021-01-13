d17d1588b27688044b03b4eccc523949ac9091e3
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-01-14 00:03:02 +0200

    fix(run): make script 'support' podman
    this should solve the issue seen in the pipeline
    
    https://github.com/containers/podman/issues/1234

--- run.sh
+++ run.sh
@@ -6,14 +6,20 @@ CONTAINER_BASE_DIR=/tmp/test
 CONTAINER_ENGINE=${CONTAINER_ENGINE:-'docker'}
 IMG=${IMG:-'quay.io/rogreen/bookmark-generator:latest'}
 OUTPUT_HTML_FILE=${OUTPUT_FILE:-'bookmark.html'}
+ADDITIONAL_CONTAINER_ENGINE_ARGS=${ADDITIONAL_CONTAINER_ENGINE_ARGS:-}
+
+if [[ "${CONTAINER_ENGINE}" == 'podman' ]]; then
+	ADDITIONAL_CONTAINER_ENGINE_ARGS="${ADDITIONAL_CONTAINER_ENGINE_ARGS} --net=host"
+fi
 
 ${CONTAINER_ENGINE} run \
     --rm \
     --interactive \
     --tty \
     --volume \
-        $(pwd):${CONTAINER_BASE_DIR}/data \
-    ${IMG} \
+        "$(pwd)":${CONTAINER_BASE_DIR}/data \
+    "${ADDITIONAL_CONTAINER_ENGINE_ARGS}" \
+    "${IMG}" \
     python3 \
     	bookmark_generator.py \
-	data/sample.yaml > ${OUTPUT_HTML_FILE}
+	data/sample.yaml > "${OUTPUT_HTML_FILE}"
