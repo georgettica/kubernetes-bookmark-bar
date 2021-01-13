5731876278e18e394d1faf90b70b64967c364381
Ron Green <11993626+georgettica@users.noreply.github.com>
2021-01-14 01:09:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,10 +8,6 @@ IMG=${IMG:-'quay.io/rogreen/bookmark-generator:latest'}
 OUTPUT_HTML_FILE=${OUTPUT_FILE:-'bookmark.html'}
 ADDITIONAL_CONTAINER_ENGINE_ARGS=${ADDITIONAL_CONTAINER_ENGINE_ARGS:-}
 
-if [[ "${CONTAINER_ENGINE}" == 'podman' ]]; then
-	ADDITIONAL_CONTAINER_ENGINE_ARGS="${ADDITIONAL_CONTAINER_ENGINE_ARGS} --net=host"
-fi
-
 ${CONTAINER_ENGINE} run \
     --rm \
     --interactive \
