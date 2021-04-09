42492c48e505559fc7fbe5729351f51555b554b5
Amin Miri <aminmiri01@gmail.com>
2021-04-09 14:04:06 +0100

    remove { } when calling variables

--- run.sh
+++ run.sh
@@ -8,14 +8,14 @@ IMG=${IMG:-'quay.io/rogreen/bookmark-generator:latest'}
 OUTPUT_HTML_FILE=${OUTPUT_FILE:-'bookmark.html'}
 ADDITIONAL_CONTAINER_ENGINE_ARGS=${ADDITIONAL_CONTAINER_ENGINE_ARGS:-}
 
-${CONTAINER_ENGINE} run \
+$CONTAINER_ENGINE run \
     --rm \
     --interactive \
     --tty \
     --volume \
-        "$(pwd)":${CONTAINER_BASE_DIR}/data \
-    "${ADDITIONAL_CONTAINER_ENGINE_ARGS}" \
+    "$(pwd)":$CONTAINER_BASE_DIR/data \
+    $ADDITIONAL_CONTAINER_ENGINE_ARGS \
     "${IMG}" \
     python3 \
-    	bookmark_generator.py \
-	data/sample.yaml > "${OUTPUT_HTML_FILE}"
+    bookmark_generator.py \
+	data/bookmark.yaml > "${OUTPUT_HTML_FILE}"
