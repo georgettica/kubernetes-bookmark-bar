d62e123b99c7bb20e5bd640bb577c9e4be8614ca
Amin Miri <aminmiri01@gmail.com>
2021-04-10 20:49:12 +0100

    Typo (#10)
    * remove { } when calling variables
    
    * remove { } when calling variables

--- run.sh
+++ run.sh
@@ -8,13 +8,13 @@ IMG=${IMG:-'quay.io/rogreen/bookmark-generator:latest'}
 OUTPUT_HTML_FILE=${OUTPUT_FILE:-'bookmark.html'}
 ADDITIONAL_CONTAINER_ENGINE_ARGS=${ADDITIONAL_CONTAINER_ENGINE_ARGS:-}
 
-${CONTAINER_ENGINE} run \
+$CONTAINER_ENGINE run \
     --rm \
     --interactive \
     --tty \
     --volume \
-        "$(pwd)":${CONTAINER_BASE_DIR}/data \
-    "${ADDITIONAL_CONTAINER_ENGINE_ARGS}" \
+    "$(pwd)":$CONTAINER_BASE_DIR/data \
+    $ADDITIONAL_CONTAINER_ENGINE_ARGS \
     "${IMG}" \
     python3 \
     	bookmark_generator.py \
